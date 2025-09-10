1017857f9aa3a51db64c896273bc4258ff4c7843
Alex Cristobal Garibay Jr. <acgaribay@mmsu.edu.ph>
2025-09-10 16:45:44 +0800

    done

---
+++ crash-course/.ipynb_checkpoints/temp_converter-checkpoint.py
@@ -0,0 +1,19 @@
+def celsius_to_fahr(temp_celsius):
+    return 9/5 * temp_celsius + 32
+
+def kelvins_to_celsius(temp_kelvins):
+    return temp_kelvins - 273.15
+
+
+def kelvins_to_fahr(temp_kelvins):
+    temp_celsius = kelvins_to_celsius(temp_kelvins)
+    temp_fahr = celsius_to_fahr(temp_celsius)
+    return temp_fahr
+
+
+def temp_calculator(temp_k, convert_to):
+    if convert_to == "C":
+        converted_temp = kelvins_to_celsius(temp_kelvins=temp_k)
+    elif convert_to == "F":
+        converted_temp = kelvins_to_fahr(temp_kelvins=temp_k)
+    return converted_temp
